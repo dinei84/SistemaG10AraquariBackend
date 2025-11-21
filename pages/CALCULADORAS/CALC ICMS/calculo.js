12f5fcb7522e1218f6ec26fe79f0a3f23439f400
Claudinei Almeida Souza <devdinei@gmail.com>
2025-11-21 15:17:52 -0300

    Adicionando icmsde pr para sp

--- pages/CALCULADORAS/CALC ICMS/calculo.js
+++ pages/CALCULADORAS/CALC ICMS/calculo.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const icmsTaxas = {
     SC: { MT: 7, MS: 7, PR: 12, SC: 17, BA: 7, TO: 7, MT: 7, PI: 7, GO: 7, MA: 7, MG: 12, RJ: 12, RS: 12, SP: 12, RO: 7, NA: 0, },
-    PR: { MT: 7, PR: 0, SC: 12 },
+    PR: { MT: 7, PR: 0, SC: 12, SP: 12 },
     SP: { SP: 0, PR: 12, SC: 12, PA: 12, BA: 12, PI: 12, GO: 7, MG: 12, RJ: 12, RS: 12, NA: 0 },
     RJ: { RJ: 0, PR: 12, SC: 12, PA: 12, BA: 12, PI: 12, GO: 12, MG: 12, RS: 12, SP: 12, NA: 0 },
     RS: { RS: 0, PR: 12, SC: 12, PA: 12, BA: 12, PI: 12, GO: 12, MG: 12, RJ: 12, SP: 12, NA: 0 },
